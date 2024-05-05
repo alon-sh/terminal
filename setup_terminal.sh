6233cf393fcc5f81cf8ac469ace09fdac4ca833d
Alon Shalev Housfater <ashalev@gmail.com>
2024-05-05 12:03:10 +0300

    update script

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -1,2 +1,19 @@
+cp .tmux.conf ..
+cp -r .config/nvim ../.config/nvim
+cp clang-format ..
 sudo apt-get update
 sudo apt-get install ripgrep tmux clang-format
+sudo dnf install ripgrep tmux clang
+
+git clone https://github.com/neovim/neovim.git
+cd neovim
+git checkout release-0.9
+
+make CMAKE_BUILD_TYPE=Release \
+     CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local"
+make install
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# go to packer.lua do :so and :PackerSync
