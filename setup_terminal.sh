8b8124edf7d308e1bfc09c6de0e48b5dac1e8e62
alon-sh <alon-h@unknown.com>
2024-05-17 15:00:26 +0300

    update setup script to speedup neovim clone, install luarocks and attempt to remove the manual step of packer sync

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -3,12 +3,11 @@ cp tmux/tmux.conf.local ~/.tmux.conf.local
 cp -r config/nvim ~/.config/nvim
 cp clang-format ~ 
 sudo apt-get update
-sudo apt-get install gettext -y ripgrep tmux clang-format gcc g++ cmake git \
+sudo apt-get install -y gettext luarocks ripgrep tmux clang-format gcc g++ cmake git \
     build-essential linux-headers-generic
 
-git clone https://github.com/neovim/neovim.git
+git clone --branch release-0.9 --depth 1 https://github.com/neovim/neovim.git
 cd neovim
-git checkout release-0.9
 
 make CMAKE_BUILD_TYPE=Release \
      CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local"
@@ -17,11 +16,15 @@ make install
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-# go to packer.lua do :so and :PackerSync
-#
 echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
 echo 'alias vi=nvim' >> ~/.bashrc
 echo 'alias vim=nvim' >> ~/.bashrc
+source ~/.bashrc
+
+# go to packer.lua do :so and :PackerSync
+# this is to download the packages
+# run :checkHealth to ensure everything was downloaded
+vi +':so | :PackerSync | :checkHealth' ~/.config/nvim/lua/alon/packer.lua
 
 # configure darkone theme for terminal
 wget https://raw.githubusercontent.com/r3tex/one-dark/master/dircolors\
