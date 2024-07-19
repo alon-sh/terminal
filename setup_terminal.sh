729d96d84dc5550cb9508e2a54558e812f1efc56
alon@alon.com <alon@example.com>
2024-07-19 18:54:36 -0400

    update to setup script

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -1,10 +1,18 @@
 cp tmux/tmux.conf ~/.tmux.conf
 cp tmux/tmux.conf.local ~/.tmux.conf.local
-cp -r config/nvim ~/.config/nvim
+mkdir -p ~/.config/nvim
+cp -r config/nvim ~/.config
 cp clang-format ~ 
 sudo apt-get update
 sudo apt-get install -y gettext luarocks ripgrep tmux clang-format gcc g++ cmake git \
     build-essential linux-headers-generic
+sudo apt-get install -y git build-essential gdb-multiarch qemu-system-misc gcc-riscv64-linux-gnu binutils-riscv64-linux-gnu
+sudo apt-get install -y build-essential bison flex gawk libncurses5-dev texinfo unzip libtool-bin gperf help2man g++ libtool automake git cvs subversion libexpat-dev libssl-dev patch curl
+sudo apt-get install -y python3
+sudo apt-get install -y bison clang flex git llvm make maven swig cmake \
+    curl g++ libx11-dev libxml2-dev libxt-dev libmotif-common libmotif-dev \
+    python3-dev zlib1g-dev llvm-dev libclang-dev libudunits2-dev \
+    libgtest-dev openjdk-11-jdk zip
 
 git clone --branch release-0.9 --depth 1 https://github.com/neovim/neovim.git
 cd neovim
@@ -24,7 +32,7 @@ source ~/.bashrc
 # go to packer.lua do :so and :PackerSync
 # this is to download the packages
 # run :checkHealth to ensure everything was downloaded
-vi +':so | :PackerSync | :checkHealth' ~/.config/nvim/lua/alon/packer.lua
+nvim +':so | :PackerSync | :checkHealth' ~/.config/nvim/lua/alon/packer.lua
 
 # configure darkone theme for terminal
 wget https://raw.githubusercontent.com/r3tex/one-dark/master/dircolors\
